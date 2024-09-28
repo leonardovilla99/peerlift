6fb5315b9392474c1f9d55957d8da0bb952a4f91
Leonardo Villa <villaleonardo99@gmail.com>
2024-09-28 12:18:29 -0400

    status bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {StyleSheet,View} from 'react-native'
 import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
 
 
 const _layout = () => {
   return (
     <Tabs screenOptions={{
-        tabBarShowLabel: true,
-        headerShown:true,
+        tabBarActiveTintColor: Colors.superBlack, 
+        tabBarInactiveTintColor: Colors.almostWhite,
+        tabBarShowLabel: false,
+        headerShown:false,
+        tabBarStyle:{
+            shadowOffset: { width: 0, height: -5 },
+            shadowColor: '#000',
+            shadowOpacity: 0.10,
+            borderWidth:0,
+        },
     }}>
         <Tabs.Screen name='home'/>
     </Tabs>
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,8 +2,14 @@ import { View, Text, SafeAreaView, StyleSheet, Image, ScrollView, StatusBar, But
 import React, { useEffect } from 'react'
 import { useAuth } from '@clerk/clerk-expo';
 import { router } from 'expo-router';
+import Colors from '@/constants/Colors';
 
 const home = () => {
+
+    useEffect(() => {
+        // Set the status bar style
+        StatusBar.setBarStyle('light-content'); // 'dark-content' for dark text on light background
+      }, []);
 
     const {signOut, isSignedIn} = useAuth();
     
@@ -12,14 +18,19 @@ const home = () => {
     }
     
     return (
-        <View>
-            {!isSignedIn && 
-                <Button title='Login' onPress={() => {onLogin()}}/>
-                }
-                {isSignedIn && 
-                <Button title='Logout' onPress={() => {signOut()}} color={'red'}/>
-                }
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false} style={{backgroundColor: Colors.superBlack}} bounces={false}>
+            <StatusBar barStyle="light-content" backgroundColor="#000000" />
+            <SafeAreaView>
+                <View>
+                    {!isSignedIn && 
+                        <Button title='Login' color={Colors.almostWhite} onPress={() => {onLogin()}}/>
+                        }
+                        {isSignedIn && 
+                        <Button title='Logout' onPress={() => {signOut()}} color={'red'}/>
+                        }
+                </View>
+            </SafeAreaView>
+        </ScrollView>
     )
 }
 
